6c8bf18baee59cd16126492e87104700b7052615
Ojas Ahirrao <ojasahirrao530@gmail.com>
2021-09-17 19:05:48 +0530

    functionality to be added

---
+++ countdown-timer/app.js
@@ -0,0 +1,11 @@
+const dayEl = document.getElementById('days')
+const hourEl = document.getElementById('hours')
+const minuteEl = document.getElementById('minutes')
+const secondEl = document.getElementById('seconds')
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
+const resetBtn = document.getElementById('reset')
+
+startBtn.addEventListener('click', () => {
+
+})
