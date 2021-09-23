52103bf8741c6507c2781921dc1d6c1c976f1959
Ojas Ahirrao <ojasahirrao530@gmail.com>
2021-09-23 21:40:51 +0530

    functionality is added

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -1,11 +1,32 @@
-const dayEl = document.getElementById('days')
-const hourEl = document.getElementById('hours')
-const minuteEl = document.getElementById('minutes')
-const secondEl = document.getElementById('seconds')
-const startBtn = document.getElementById('start')
-const stopBtn = document.getElementById('stop')
-const resetBtn = document.getElementById('reset')
+const countdownEl = document.getElementById('countdown')
 
-startBtn.addEventListener('click', () => {
+setInterval(() => {
 
-})
+    const currentTime = new Date().getTime()
+
+    const currentYear = new Date().getFullYear()
+    const currentMonth = new Date().getMonth()
+    const currentDate = new Date().getDate()
+
+    const currentHours = new Date().getHours()
+    const currentMinutes = new Date().getMinutes()
+
+    const countdownTime = new Date(currentYear, currentMonth, currentDate + 7, currentHours, currentMinutes, 0).getTime()
+
+    const distance = countdownTime - currentTime
+
+    const days = Math.floor(distance / (24 * 60 * 60 * 1000))
+
+    const hours = Math.floor((distance) % (24 * 60 * 60 * 1000) / (60 * 60 * 1000))
+
+    const minutes = Math.floor((distance % (60 * 60 * 1000)) / (60 * 1000))
+
+    const seconds = Math.floor((distance % (60 * 1000)) / (1000))
+
+    countdownEl.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+
+    if (distance < 0) {
+        countdownEl.innerHTML = 'The timer has ended'
+    }
+
+}, 1000)
